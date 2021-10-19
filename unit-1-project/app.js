e80de42a89dbb87f60f05e073def0e3776f3e3fa
Maurrii <ahmauri.n@gmail.com>
2021-10-19 12:23:52 -0700

    pushing project

--- unit-1-project/app.js
+++ unit-1-project/app.js
@@ -1,18 +1,25 @@
 /*-------------------------------- Constants --------------------------------*/
-const matchCombos = { [3, 6]: [2, 7], [1, 8]: [4,9]
-}
+const matchCombos = [
+  [0, 7], [1, 6], [2, 5], [3,8]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
-
+const resetBtn = document.getElementById('reset-button')
+const cards = document.querySelectorAll('.cards')
+const messageEl = document.querySelector('#message')
 /*----------------------------- Event Listeners -----------------------------*/
-
+cards.forEach((card) => card.addEventListener('click', handleClick))
 
 
 /*-------------------------------- Functions --------------------------------*/
+function handleClick(evt){
+}
+if (!hasFlippedCard) {
+    hasFlippedCard = true;
+  }
