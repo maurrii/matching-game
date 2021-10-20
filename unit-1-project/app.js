168f0851d901873bf641473b5cf18ad0cd1b36ce
Maurrii <ahmauri.n@gmail.com>
2021-10-20 13:04:32 -0700

    end of the day

--- unit-1-project/app.js
+++ unit-1-project/app.js
@@ -4,85 +4,106 @@ const matchCombos = [
 ]
 
 const cardArray = [
-	{
-		name: 'blue',
+  {
+    name: 'blue',
 		img: 'images/blue star.png',
-		status: hidden,
 	},
 	{
-		name: 'blue',
+    name: 'blue',
 		img: 'images/blue star.png',
-		status: hidden,
 	},
 	{
-		name: 'pink',
+    name: 'pink',
 		img: 'images/pink star.png',
-    status:hidden,
 	},
 	{
-		name: 'pink',
+    name: 'pink',
 		img: 'images/pink star.png',
-    status:hidden,
 	},
 	{
-		name: 'silver',
+    name: 'silver',
 		img: 'images/silver star.png',
-    status:hidden,
 	},
 	{
-		name: 'silver',
+    name: 'silver',
 		img: 'images/silver star.png',
-    status:hidden,
 	},
 	{
-		name: 'red',
+    name: 'red',
 		img: 'images/red star.png',
-    status:hidden,
 	},
 	{
-		name: 'red',
+    name: 'red',
 		img: 'images/red star.png',
-    status:hidden,
 	},
 ]
 /*---------------------------- Variables (state) ----------------------------*/
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
+let cardsChosen = []
+
+
 
 /*------------------------ Cached Element References ------------------------*/
+
+const optionOne = cardsChosenId[0]
+
+const optionTwo = cardsChosenId[2]
+
 const resetBtn = document.getElementById('reset-button')
+
 const cards = document.querySelectorAll('.cards')
-console.dir(cards)
+
 const messageEl = document.querySelector('#message')
+
+const frontCard = document.querySelectorAll(".front")
+
+
 /*----------------------------- Event Listeners -----------------------------*/
 cards.forEach((card) => card.addEventListener('click', handleClick))
 
-messageEl.addEventListener
+messageEl.addEventListener()
 /*-------------------------------- Functions --------------------------------*/
+// checks for match
+function checkForMatch(){
+
+}
+
+
+
+
+
+
+
+// flips a card
 function handleClick(evt){
   let backCard = evt.target
   let frontCard = evt.srcElement.nextElementSibling
   backCard.style.display = "none"
 frontCard.style.display = "block"
-
-
+cardsChosen.push(cardArray[cardId].name)
+cardsChosenId.push(cardId)
+if (cardsChosen.length === 2){
+  setTimeout(checkForMatch, 500)
 }
+}
+
 // if (!hasFlippedCard) {
-//     hasFlippedCard = true
-//     firstCard = this
-//   } else {
-//     hasFlippedCard = false
-//     secondCard = this
-//   }
-//   if (firstCard === true && secondCard === true) {
-//     firstCard.removeEventListener("click", handleClick)
-//     secondCard.removeEventListener('click', handleClick)
-//   } else {
-//     setTimeout(()=> {
-//       firstCard.classList.remove('flip')
-//     },1500)
-//   }
-
-
-  resetBtn.addEventListener('click', handleClick)
+  //     hasFlippedCard = true
+  //     firstCard = this
+  //   } else {
+    //     hasFlippedCard = false
+    //     secondCard = this
+    //   }
+    //   if (firstCard === true && secondCard === true) {
+      //     firstCard.removeEventListener("click", handleClick)
+      //     secondCard.removeEventListener('click', handleClick)
+      //   } else {
+        //     setTimeout(()=> {
+          //       firstCard.classList.remove('flip')
+          //     },1500)
+          //   }
+          
+          
+          resetBtn.addEventListener('click', handleClick)
