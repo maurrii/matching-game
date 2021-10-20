bd6400aedcf63d207a4fc3ff20030a2825532ef2
Maurrii <ahmauri.n@gmail.com>
2021-10-19 17:45:40 -0700

    got my cards to flip

--- unit-1-project/app.js
+++ unit-1-project/app.js
@@ -3,7 +3,7 @@ const matchCombos = [
   [0, 7], [1, 6], [2, 5], [3,8]
 ]
 
-
+const playerArray = []
 /*---------------------------- Variables (state) ----------------------------*/
 let hasFlippedCard = false;
 let firstCard, secondCard;
@@ -12,6 +12,7 @@ let firstCard, secondCard;
 /*------------------------ Cached Element References ------------------------*/
 const resetBtn = document.getElementById('reset-button')
 const cards = document.querySelectorAll('.cards')
+console.dir(cards)
 const messageEl = document.querySelector('#message')
 /*----------------------------- Event Listeners -----------------------------*/
 cards.forEach((card) => card.addEventListener('click', handleClick))
@@ -19,11 +20,28 @@ cards.forEach((card) => card.addEventListener('click', handleClick))
 messageEl.addEventListener
 /*-------------------------------- Functions --------------------------------*/
 function handleClick(evt){
+  console.log(evt) 
+  let backCard = evt.target
+  let frontCard = evt.srcElement.nextElementSibling
+  backCard.style.display = "none"
+frontCard.style.display = "block"
 
+
+// if (!hasFlippedCard) {
+//     hasFlippedCard = true
+//     firstCard = this
+//   } else {
+//     hasFlippedCard = false
+//     secondCard = this
+//   }
+//   if (firstCard.handleClick === secondCard.handleClick) {
+//     firstCard.removeEventListener("click", handleClick)
+//     secondCard.removeEventListener('click', handleClick)
+//   } else {
+//     setTimeout(()=> {
+//       firstCard.classList.remove('flip')
+//     },1500)
+//   }
 }
-if (!hasFlippedCard) {
-    hasFlippedCard = true;
-  }
 
-
-  resetBtn.addEventListener('click', init)
+  resetBtn.addEventListener('click', handleClick)
